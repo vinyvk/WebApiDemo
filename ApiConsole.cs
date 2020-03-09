e42a8f50846fd655f6c890b4c19da44866f542ce
vinyvk <vinaykumargk139@gmail.com>
2020-03-09 12:00:27 +0530

    Console api to call other api

---
+++ ApiConsole.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace HttpClientSample
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        static void ShowProduct(Product product)
+        {
+            Console.WriteLine($"Name: {product.Name}\tPrice: " +
+                $"{product.Price}\tCategory: {product.Category}");
+        }
+        static async Task<Product> GetProductAsync(string path)
+        {
+            Product product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsAsync<Product>();
+            }
+            return product;
+        }
+        static async Task<Uri> CreateProductAsync(Product product)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "api/Product", product);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static async Task RunSync()
+        {
+            client.BaseAddress = new Uri("https://localhost:44349/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            Product prod = new Product();
+            var url = await CreateProductAsync(prod);
+            prod = await GetProductAsync(url.PathAndQuery);
+            ShowProduct(prod);
+
+        }
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
